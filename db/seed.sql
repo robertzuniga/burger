b23c97cab98de37c8faa5de5af4fa85f95af17ad
Robert Zuniga <github@mastertheday.com>
2019-08-24 12:13:07 -0400

    added mysql schema and seeds for burgers_db

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+-- Robert Zuniga seeds.sql
+
+-- USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('big mac');
+INSERT INTO burgers (burger_name) VALUES ('whooper');
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('plain cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('garlic cheeseburger', true);
+INSERT INTO burgers (burger_name) VALUES ('hamburger');
